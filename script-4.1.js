44dba0469340174289985e87d21e7bea14495b7f
Francisco Arnoldo <103003088+Franx12@users.noreply.github.com>
2022-06-22 19:22:01 -0400

    Tarea N°4

---
+++ script-4.1.js
@@ -0,0 +1,25 @@
+//Mostrar en consola los números del 1 al 100 de 1 en 1:
+
+/*
+function sumaNumeros() {
+  for(let index = 1; index <= 100; index++) {
+    console.log(index);
+  }
+}
+
+sumaNumeros();
+*/
+
+
+//Mostrar en consola los números del 1000 al 500 de 2 en 2:
+
+/*
+const restaNumeros = (a) => {
+  if(a < 500) return;
+  console.log(a);
+  return restaNumeros(a - 2);
+}
+
+restaNumeros(1000);
+*/
+
---
+++ script-4.2.js
@@ -0,0 +1,17 @@
+//Obtener el promedio de notas de un alumno: 
+
+/*la suma de notas debe ser el retorno de una función y el promedio
+el retorno de otra función. Las notas son: 6,8,9,2,5,10.*/
+
+//Primer intento:
+/*
+let misNotas = [6, 8, 9, 2, 5, 10];
+
+let suma = misNotas.reduce((a, n) => (a += n, a), 0);
+console.log(suma);
+
+let promedio = suma /misNotas.length;
+console.log(promedio);
+*/
+
+
